75e85f3d75ff65e9ff6c809266d887244bd9f3e6
Brandon Satrom <bsatrom+gh@gmail.com>
2013-06-06 16:37:45 -0500

    added basic widget definition for Form; closes #4

--- src/kendo.forms.js
+++ src/kendo.forms.js
@@ -1,3 +1,21 @@
 (function($, kendo) {
-	console.log('hai');
+	var ui = kendo.ui,
+		Widget = ui.Widget,
+		formWidget;
+
+	Form = Widget.extend({
+		init: function(element, options) {
+
+			// base call to initialize the widget
+			Widget.fn.init.call(this, element, options);
+		},
+
+		options: {
+			// the name is what it will appear in the kendo namespace (kendo.ui.Form).
+			// The jQuery plugin would be jQuery.fn.kendoForm.
+			name: "Form"
+		}
+	});
+
+	ui.plugin(Form);
 } (jQuery, kendo));
--- test/js/forms.js
+++ test/js/forms.js
@@ -7,10 +7,14 @@ $(function($, kendo) {
 		}
 	};
 
-	describe('Foobar', function() {
+	describe('Form Widget initialization', function() {
 		describe('#sayHello()', function() {
-			it('should return some text', function() {
-				assert.equal(foobar.sayHello(), 'Hello World!');
+			it('should exist in the kendo.ui namespace', function() {
+				assert.ok(kendo.ui.Form, 'Form object is not available on the kendo.ui namespace');
+			});
+
+			it('should be able to perform imperative initialization with JavaScript', function() {
+				assert.ok($('<input></input>').kendoForm, 'widget kendoForm is not available on the Kendo object');
 			});
 		});
 	});

5bdb91df28e377f29b34113267ae8d5048bd5b52
Brandon Satrom <bsatrom+gh@gmail.com>
2013-08-26 16:26:17 -0500

    fixed testling runner

--- spec/js/runner.js
+++ spec/js/runner.js
@@ -1,24 +1,8 @@
 (function() {
   var jasmineEnv = jasmine.getEnv();
-  jasmineEnv.updateInterval = 1000;
-
-  var htmlReporter = new jasmine.HtmlReporter();
-  jasmineEnv.addReporter(htmlReporter);
 
   var tapReporter = new jasmine.TapReporter();
   jasmineEnv.addReporter(tapReporter);
-
-  var currentWindowOnload = window.onload;
-
-  window.onload = function() {
-    if (currentWindowOnload) {
-      currentWindowOnload();
-    }
-    execJasmine();
-  };
-
-  function execJasmine() {
-    jasmineEnv.execute();
-  }
-
+  jasmineEnv.execute();
+  
 })();
